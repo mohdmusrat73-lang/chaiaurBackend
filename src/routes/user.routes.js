ea42934414f834f9cd8eac1ab1937c9bb347bb31
Mohd Musrat <kamranahamad778@gmail.com>
2026-01-11 16:36:27 +0530

    add user account management routes: change password, fetch current user, update details, and manage avatars

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,18 @@
-import {Router } from "express"
-import { loginUser, logOutUser, registerUser, refreshAccessToken } from "../controllers/user.controllers.js";
-import {upload} from "../middlewares/multer.middlewares.js";
+import { Router } from "express"
+import {
+    loginUser,
+    logOutUser,
+    registerUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
+} from "../controllers/user.controllers.js";
+import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
@@ -24,7 +36,15 @@ router.route("/login").post(loginUser);
 // secure route
 
 router.route("/logout").post(verifyJWT, logOutUser)
-
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+// we taking values from param to we use below syntax for rounting 
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/watch-history").get(verifyJWT, getWatchHistory)
 
 export default router;
